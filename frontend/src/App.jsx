e24e62c7fc1d664c3b62848a77b1b41d4ce47771
Kemelite1 <osikemelite42@gmail.com>
2024-03-18 17:22:48 +0100

    Refactor routing using react router

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,24 @@
-import Header from './components/Header/Header'
+import { BrowserRouter, Route, Routes  } from 'react-router-dom';
+import AppLayout from './pages/AppLayout';
+import HomePage from './pages/HomePage';
+import AboutPage from './pages/AboutPage';
+import Locations from './pages/Locations';
+import PageNotFound from './pages/PageNotFound';
 
 function App() {
   return (
-    <div>
-        <Header />
-    </div>
+    <>
+    <BrowserRouter>
+    <Routes>
+    <Route path="/" element={<AppLayout />}>
+        <Route index element={<HomePage />} />
+        <Route path="Locations" element={<Locations />} />
+        <Route path="/about" element={<AboutPage />} />
+        <Route path="*" element={<PageNotFound />} />
+    </Route>
+    </Routes>
+    </BrowserRouter>
+    </>
   )
 }
 
