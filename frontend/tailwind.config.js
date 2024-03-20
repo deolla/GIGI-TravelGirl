9e9601dc8924131470c0c104f42bc70a2d79281b
Kemelite1 <osikemelite42@gmail.com>
2024-03-20 18:05:27 +0100

    Refactor TailwindCSS container padding in tailwind.config.js
    - Updated container padding values for different screen sizes
      - Adjusted padding values for better consistency and responsiveness across screen sizes

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -5,7 +5,23 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#5356FF',
+        secondary: '#FF407D',
+      },
+      container: {
+        center: true,
+        padding: {
+          DEFAULT: '1rem',
+          sm: '2rem',
+          lg: '4rem',
+          xl: '5rem',
+          '2xl': '6rem',
+        }
+
+      }
+    },
   },
   plugins: [],
 }
