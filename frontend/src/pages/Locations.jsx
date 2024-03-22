52170e6cce8b42435df8faf2af38f0f1e12a34c4
Kemelite1 <osikemelite42@gmail.com>
2024-03-22 18:06:28 +0100

    Added a new page component called Location, which renders the Locations component

---
+++ frontend/src/pages/Locations.jsx
@@ -0,0 +1,11 @@
+import Locations from '../components/Locations/Locations';
+
+function Location() {
+  return (
+    <div className='pt-14'>
+      <Locations />
+    </div>
+  )
+}
+
+export default Location
