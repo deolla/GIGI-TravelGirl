c80e54e5d3cceb549dbffebfa1f52280648f8583
Kemelite1 <osikemelite42@gmail.com>
2024-04-01 13:55:56 +0100

    Add slick-carousel theme CSS import to Testimonials component

--- frontend/src/components/Testimonials/Testimonials.jsx
+++ frontend/src/components/Testimonials/Testimonials.jsx
@@ -1,6 +1,6 @@
-import React from "react";
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 const testimonialData = [
   {
@@ -66,9 +66,7 @@ const Testimonials = () => {
         <div className="container">
           {/* Header section */}
           <div className="text-center mb-20 max-w-[400px] mx-auto">
-            <p className="text-sm bg-clip-text text-transparent bg-gradient-to-r from-primary to-secondary">
-              Testimonial
-            </p>
+            
             <h1 className="text-3xl font-bold">Testimonial</h1>
             <p className="text-xs text-gray-400">
               {" "}
